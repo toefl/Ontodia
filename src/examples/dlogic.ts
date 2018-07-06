c14d9f6735b3dc9d8257a258cdebaad24d8d76b7
Айдар Бикмухаметов <ARBikmuhametov@1cbit.ru>
2018-07-06 15:50:43 +0300

    The truth table is being worked on.

--- src/examples/dlogic.ts
+++ src/examples/dlogic.ts
@@ -5,40 +5,59 @@ import { Workspace, WorkspaceProps } from '../index';
 
 import { onPageLoad, tryLoadLayoutFromLocalStorage, saveLayoutToLocalStorage } from './common';
 import { DemoDataProvider } from '../index';
-import {
-    Dictionary, ClassModel, LinkType, ElementModel, LinkModel, LinkCount,
-    ElementIri, ClassIri, LocalizedString
-} from '../ontodia/data/model';
-import { FatClassModel } from '../ontodia/diagram/elements';
-import { lab } from 'd3-color';
+import { ClassModel, ClassIri } from '../ontodia/data/model';
 
 const data = require<string>('./resources/logicalExpression.txt');
 const LINK_TYPES = require<any>('./resources/linkTypes.json');
 const ELEMENTS = require<any>('./resources/elements.json');
-const LINKS  = require<any>('./resources/links.json');
+const LINKS = require<any>('./resources/links.json');
 
-function LEParser(data: string): DemoDataProvider {
-    const symbols = ['⊑', '∃', '⊓', '≡', '¬'];
-    const concepts = data.split(' ');
+function DeleteEmptyCells(concepts: Array<string>): Array<string> {
     for (let i = 0; i < concepts.length; i++) {
-        if (!symbols.indexOf(concepts[i])) {
+        if (concepts[i].length === 0) {
             concepts.splice(i, 1);
             i--;
         }
     }
+    return concepts;
+}
+
+function CreateTruthTable(formula: string, concepts: Array<string>): Array<string> {
+    const truthTable = new Array(concepts.length);
+    const formulaLetters = formula.split('');
+    const operations: Array<string> = [];
+    formulaLetters.forEach(letter => {
+        if (letter === '⊑' || letter === '∃' || letter === '⊓' || letter === '≡' || letter === '¬') {
+            operations.push(letter);
+        }
+    });
+    if (operations.length === 0) { return undefined; }
+    for (let i = 0; i < truthTable.length; i++) {
+        truthTable[i] = new Array(concepts.length + operations.length);
+    }
+    return truthTable;
+}
+
+function LEParser(formula: string): DemoDataProvider {
+    let concepts = formula.split(/[ ⊑ | ∃ | ⊓ | ≡ | ¬ | \( | \)]/g);
+    concepts = DeleteEmptyCells(concepts);
     const treeConcepts: ClassModel[] = [];
     concepts.forEach(concept => {
-        let count = 0;
-        const children: ClassModel[] = [];
-        const id = concept as ClassIri;
-        const label: { values: LocalizedString[] } = { values: [{ lang: 'eng', text: concept }] };
-        const tmp: ClassModel = { id, label, count, children };
-        console.log(tmp);
+        const tmp: ClassModel = {
+            id: concept as ClassIri,
+            label: { values: [{ lang: 'eng', text: concept }] },
+            count: 0,
+            children: [],
+        };
         treeConcepts.push(tmp);
     });
+    const truthTable: Array<string> = CreateTruthTable(formula, concepts);
+    if (truthTable === undefined) { alert('The original formula does not contain logical operations'); }
+    console.log(truthTable);
     const dataProvider = new DemoDataProvider(treeConcepts, LINK_TYPES, ELEMENTS, LINKS);
     return dataProvider;
 }
+
 function onWorkspaceMounted(workspace: Workspace) {
     if (!workspace) { return; }
 
